8c55ae9272a655b85da57c189cb7b51530fb2a2d
unknown <darshdoshi01@gmail.com>
2024-02-20 18:50:40 +0530

    Changes In DropDownList Style

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 function changeSelectedColor(ddl, index) {
-    if (index == 0)
-        ddl.style.backgroundColor = "#f86c6b";
-    else if (index == 1)
-        ddl.style.backgroundColor = "#f8cb00";
-    else if (index == 2)
-        ddl.style.backgroundColor = "#4dbd74";
+    if (index == 0) {
+        ddl.style.backgroundColor = "#ffdada";
+        ddl.style.color = "#f86c6b";
+    }
+
+    else if (index == 1) {
+        ddl.style.backgroundColor = "#fff0ab";
+        ddl.style.color = "#f8cb00";
+    }
+    else if (index == 2) {
+        ddl.style.backgroundColor = "#cfedda";
+        ddl.style.color = "#4dbd74";
+    }
+    ddl.style.fontWeight = 800;
 }
 
 let ddl = document.getElementById('task-priority');
