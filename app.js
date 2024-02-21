31fb843c2aa5489aa3bfdf3bc07637f05ac98178
Darsh Doshi <darshdoshi01@gmail.com>
2024-02-21 19:09:48 +0530

    Gave Proper Comments

--- app.js
+++ app.js
@@ -3,22 +3,30 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const User = require('./models/user');
 const path = require('path')
+
 const app = express()
 const port = 80
 
+// Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/taskpulse');
 
+// Set up static files path
 let staticFilesPath = path.join(__dirname, '/public');
-console.log(staticFilesPath);
 app.use(express.static(staticFilesPath));
+
+// Use body-parser middleware for URL-encoded data
 app.use(bodyParser.urlencoded({ extended: true })); // Use body-parser for URL-encoded data
 
+// Route to serve the login/signup page
 app.get('/', (req, res) => {
     let fileName = path.join(__dirname, 'views', 'authentication.html');
     res.sendFile(fileName);
 })
 
+// Route to handle user registration
 app.post('/register', (req, res) => {
+
+    // Create a new user using the User model
     const newUser = new User({
         name: req.body.name,
         email: req.body.email,
@@ -26,6 +34,7 @@ app.post('/register', (req, res) => {
     })
     newUser.save()
         .then(() => {
+            // After successful registration, redirect to the index page
             let fileName = path.join(__dirname, 'views', 'index.html');
             res.status(201).sendFile(fileName);
         })
@@ -35,6 +44,7 @@ app.post('/register', (req, res) => {
         });
 })
 
+// Start the server
 app.listen(port, () => {
     console.log(`http://127.0.0.1:${port}/`)
 })
